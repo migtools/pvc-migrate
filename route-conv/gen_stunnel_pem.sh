50a871dad6f00c503659583d1d3287454318047f
Erik Nelson <erik@nsk.io>
2020-06-29 12:27:20 -0400

    Create stunnel.pem in local dir

--- route-conv/gen_stunnel_pem.sh
+++ route-conv/gen_stunnel_pem.sh
@@ -4,7 +4,7 @@ umask 77
 PEM1=`/bin/mktemp /tmp/openssh.XXXXXX`
 PEM2=`/bin/mktemp /tmp/openssh.XXXXXX`
 /usr/bin/openssl req -utf8 -newkey rsa:2048 -keyout $PEM1 -nodes -x509 -days 365 -out $PEM2
-cat $PEM1 >  stunnel.pem
-echo "" >> stunnel.pem
-cat $PEM2 >> stunnel.pem
+cat $PEM1 >  $_dir/stunnel.pem
+echo "" >> $_dir/stunnel.pem
+cat $PEM2 >> $_dir/stunnel.pem
 rm -f $PEM1 $PEM2
