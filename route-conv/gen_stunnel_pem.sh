4e0973d953a1bc4b1ad7ff4bfda8641119b14021
Erik Nelson <erik@nsk.io>
2020-06-29 12:21:55 -0400

    Add gen_stunnel_pem.sh

---
+++ route-conv/gen_stunnel_pem.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+umask 77
+PEM1=`/bin/mktemp /tmp/openssh.XXXXXX`
+PEM2=`/bin/mktemp /tmp/openssh.XXXXXX`
+/usr/bin/openssl req -utf8 -newkey rsa:2048 -keyout $PEM1 -nodes -x509 -days 365 -out $PEM2
+cat $PEM1 >  stunnel.pem
+echo "" >> stunnel.pem
+cat $PEM2 >> stunnel.pem
+rm -f $PEM1 $PEM2
