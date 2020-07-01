e30a5df3da28087ac349c0176f7d22728d9e854b
Derek Whatley <derekwhatley@gmail.com>
2020-07-01 11:30:20 -0400

    Use tempfile instead of /bin/tempfile in stunnel script, update .gitignore

--- 3_run_rsync/scripts/setup-stunnel.sh
+++ 3_run_rsync/scripts/setup-stunnel.sh
@@ -2,8 +2,8 @@
 
 _dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 umask 77
-PEM1=`/bin/mktemp /tmp/openssh.XXXXXX`
-PEM2=`/bin/mktemp /tmp/openssh.XXXXXX`
+PEM1=`mktemp /tmp/openssh.XXXXXX`
+PEM2=`mktemp /tmp/openssh.XXXXXX`
 /usr/bin/openssl req -utf8 -newkey rsa:2048 -keyout $PEM1 -nodes -x509 -days 365 -out $PEM2 -subj "/C=US/ST=NC/L=RDU/O=migration/CN=openshift.io"
 cat $PEM1 >  $_dir/tls.key
 cat $PEM2 > $_dir/tls.crt
